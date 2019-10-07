28c9d72b302cfc3a24c71d3b11b836cf695cfe2a
hobo <1056024860@qq.com>
2019-10-07 09:31:29 +0800

    修改了一些bug&调整redis位置

--- src/main/java/com/swpu/uchain/blog/util/AliyunSmsUtils.java
+++ src/main/java/com/swpu/uchain/blog/util/AliyunSmsUtils.java
@@ -21,8 +21,8 @@ public class AliyunSmsUtils {
     private static final String PRODUCT = "Dysmsapi";
     private static final String DOMAIN = "dysmsapi.aliyuncs.com";
 
-    private static final String ACCESS_KEY_ID = "LTAIOqY6ewyldbO7";
-    private static final String ACCESS_KEY_SECRET = "tO28iD4zgRBwnAGw3h87uoi0kgr3oq";
+    private static final String ACCESS_KEY_ID = "";
+    private static final String ACCESS_KEY_SECRET = "";
 
     public static boolean sendInsertUserMsg(String telephone, String code,String templateCode) throws ClientException {
 
