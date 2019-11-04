40931e4e8a7df6448c65aa0826aa5da55a58fbb5
hobo <1056024860@qq.com>
2019-11-04 21:23:56 +0800

    处理跨域问题

--- src/main/java/com/swpu/uchain/blog/controller/LoginController.java
+++ src/main/java/com/swpu/uchain/blog/controller/LoginController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 @RequestMapping("/anon")
 @Api(tags = "登录注册")
+@CrossOrigin
 public class LoginController {
     @Autowired
     private UserService userService;
